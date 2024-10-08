7fb7eb2e3c2ea7fce1c503a908e541323c4696f8
Junior Pezantes <ti15@emilima.com.pe>
2024-10-07 20:12:23 -0500

    Add: Component/Nav

---
+++ src/data/navData.js
@@ -0,0 +1,14 @@
+const navData = [
+  {
+    name: "About",
+    path: "/about/",
+    icon: "bx bxs-info-circle"
+  },
+  {
+    name: "Blog",
+    path: "/blog/",
+    icon: "bx bxs-message-dots"
+  }
+]
+
+export default navData;
