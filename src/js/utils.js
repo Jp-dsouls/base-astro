f1f043012dbf05945e0785db8b022d06d1e72b08
Junior Pezantes <ti15@emilima.com.pe>
2024-10-09 22:24:44 -0500

    Fix: Pages/Blog

--- src/js/utils.js
+++ src/js/utils.js
@@ -6,3 +6,30 @@ export function formateDate(date) {
     timeZone: "UTC",
   })
 }
+
+export function formatBlogPosts(posts, {
+  filterOutDrafts = true,
+  filterOutFuturePosts = true,
+  sortByDate = true,
+  limit = undefined
+} = {}) {
+  const filteredPosts = posts.reduce((acc, post) => {
+    const { date, draft } = post.frontmatter;
+    if (filterOutDrafts && draft) return acc;
+    if (filterOutFuturePosts && new Date(date) > new Date()) return acc;
+    acc.push(post)
+    return acc
+  }, [])
+  //Sort by Date or randomize
+  if (sortByDate) {
+    filteredPosts.sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
+    )
+  } else {
+    filteredPosts.sort(() => Math.random() - 0.5)
+  }
+  //Limit if number is passed
+  if (typeof limit === "number") {
+    return filteredPosts.slice(0, limit);
+  }
+  return filteredPosts
+}
